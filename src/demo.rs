7db5d4822596c1d234c56943ec7e561b135d682c
Gary Yu <gairy.yu@gmail.com>
2018-07-28 16:36:17 +0800

    demo: complete wiki/Transaction-Aggregation and related demo code

--- src/demo.rs
+++ src/demo.rs
@@ -418,4 +418,200 @@ mod tests {
             println!("\n\"subset sum\" verify NOK:\toutput1+output2 != input+excess");
         }
     }
+
+    #[test]
+    fn test_demo_aggregate_transactions() {
+        let secp = Secp256k1::with_caps(ContextFlag::Commit);
+
+        fn commit(value: u64, blinding: SecretKey) -> Commitment {
+            let secp = Secp256k1::with_caps(ContextFlag::Commit);
+            secp.commit(value, blinding).unwrap()
+        }
+
+        //------	transaction 1		-----//
+
+        println!("\nFirst transaction...\n");
+
+        let mut r1 = SecretKey([0;32]);
+        let mut r2 = SecretKey([0;32]);
+        let mut r3 = SecretKey([0;32]);
+
+        r1.0[31] = 113;     // input
+        r3.0[31] = 42;      // for change
+        r2.0[31] = 113-42;  // total blinding factor from sender
+
+        // split original r=28 into k1+k2
+        let mut k1 = SecretKey([0;32]);
+        let mut k2 = SecretKey([0;32]);
+        k1.0[31] = 13;
+        k2.0[31] = 15;
+
+        let input = commit(3, r1);
+        let output1 = commit(2, secp.blind_sum(vec![r2, k1, k2], vec![]).unwrap());
+        let output2 = commit(1, r3);
+        let tmp = commit(2, secp.blind_sum(vec![r2, k1], vec![]).unwrap());
+
+        // publish k1*G as excess and k2, instead of (k1+k2)*G
+        let excess = secp.commit_sum(vec![tmp, output2], vec![input]).unwrap();
+
+        println!("  input=113*G+3*H:\t{:?}\noutput1= 99*G+2*H:\t{:?}\noutput2= 42*G+1*H:\t{:?}",
+                 input,
+                 output1,
+                 output2,
+        );
+
+        // sign it only with k1 instead of (k1+k2)
+
+        let mut msg = [0u8; 32];
+        thread_rng().fill_bytes(&mut msg);
+        let msg = Message::from_slice(&msg).unwrap();
+
+        let sig = secp.sign(&msg, &k1).unwrap();
+
+        let pubkey = excess.to_pubkey(&secp).unwrap();
+
+        println!("\n\tmsg:\t\t{:?}\n\texcess w/ k1*G:\t{:?}\n\tSignature:\t{:?}\n\tk2:\t\t{:?}",
+                 msg,
+                 excess,
+                 sig,
+                 k2,
+        );
+
+        // check that we can successfully verify the signature with the public key
+        if let Ok(_) = secp.verify(&msg, &sig, &pubkey) {
+            println!("Signature verify OK");
+        } else {
+            println!("Signature verify NOK");
+        }
+
+        if true==secp.verify_commit_sum(
+            vec![output1, output2],
+            vec![input, excess],
+        ){
+            println!("\n\"subset sum\" verify OK:\toutput1+output2 = input+excess");
+        }else{
+            println!("\n\"subset sum\" verify NOK:\toutput1+output2 != input+excess");
+        }
+
+        if true==secp.verify_commit_sum(
+            vec![output1, output2],
+            vec![input, excess, commit(0, k2)],
+        ){
+            println!("\nsum with k2*G verify OK:\toutput1 + output2 = input + excess + k2*G");
+        }else{
+            println!("\nsum with k2*G verify NOK:\toutput1 + output2 != input + excess + k2*G");
+        }
+
+        //------	transaction 2		-----//
+
+        println!("\nSecond transaction...\n");
+
+        let mut new_r1 = SecretKey([0;32]);
+        let mut new_r2 = SecretKey([0;32]);
+        let mut new_r3 = SecretKey([0;32]);
+
+        new_r1.0[31] = 205;     // input
+        new_r3.0[31] = 68;      // for change
+        new_r2.0[31] = 205-68;  // total blinding factor from sender
+
+        // split original r=79 into k1+k2
+        let mut new_k1 = SecretKey([0;32]);
+        let mut new_k2 = SecretKey([0;32]);
+        new_k1.0[31] = 55;
+        new_k2.0[31] = 24;
+
+        let new_input = commit(10, new_r1);
+        let new_output1 = commit(6, secp.blind_sum(vec![new_r2, new_k1, new_k2], vec![]).unwrap());
+        let new_output2 = commit(4, new_r3);
+        let new_tmp = commit(6, secp.blind_sum(vec![new_r2, new_k1], vec![]).unwrap());
+
+        // publish k1*G as excess and k2, instead of (k1+k2)*G
+        let new_excess = secp.commit_sum(vec![new_tmp, new_output2], vec![new_input]).unwrap();
+
+        println!("  input=205*G+10*H:\t{:?}\noutput1= 216*G+6*H:\t{:?}\noutput2= 68*G+4*H:\t{:?}",
+                 new_input,
+                 new_output1,
+                 new_output2,
+        );
+
+        // sign it only with k1 instead of (k1+k2)
+
+        let mut new_msg = [0u8; 32];
+        thread_rng().fill_bytes(&mut new_msg);
+        let new_msg = Message::from_slice(&new_msg).unwrap();
+
+        let new_sig = secp.sign(&new_msg, &new_k1).unwrap();
+
+        let new_pubkey = new_excess.to_pubkey(&secp).unwrap();
+
+        println!("\n\tmsg:\t\t{:?}\n\texcess w/ k1*G:\t{:?}\n\tSignature:\t{:?}\n\tk2:\t\t{:?}",
+                 new_msg,
+                 new_excess,
+                 new_sig,
+                 new_k2,
+        );
+
+        // check that we can successfully verify the signature with the public key
+        if let Ok(_) = secp.verify(&new_msg, &new_sig, &new_pubkey) {
+            println!("Signature verify OK");
+        } else {
+            println!("Signature verify NOK");
+        }
+
+        if true==secp.verify_commit_sum(
+            vec![new_output1, new_output2],
+            vec![new_input, new_excess],
+        ){
+            println!("\n\"subset sum\" verify OK:\toutput1+output2 = input+excess");
+        }else{
+            println!("\n\"subset sum\" verify NOK:\toutput1+output2 != input+excess");
+        }
+
+        if true==secp.verify_commit_sum(
+            vec![new_output1, new_output2],
+            vec![new_input, new_excess, commit(0, new_k2)],
+        ){
+            println!("\nsum with k2*G verify OK:\toutput1 + output2 = input + excess + k2*G");
+        }else{
+            println!("\nsum with k2*G verify NOK:\toutput1 + output2 != input + excess + k2*G");
+        }
+
+        //------	aggregate transactions		-----//
+
+        println!("\naggregate these 2 transactions...\n");
+
+        println!(" input1=113*G+3*H:\t{:?}\n input2=205*G+10*H:\t{:?}\noutput1= 99*G+2*H:\t{:?}\noutput2= 42*G+1*H:\t{:?}",
+                 input,
+                 new_input,
+                 output1,
+                 output2,
+        );
+        println!("output3= 216*G+6*H:\t{:?}\noutput4= 68*G+4*H:\t{:?}",
+                 new_output1,
+                 new_output2,
+        );
+        println!("\n\tmsg1:\t\t{:?}\n\texcess1:\t{:?}\n\tSignature1:\t{:?}",
+                 msg,
+                 excess,
+                 sig,
+        );
+        println!("\n\tmsg2:\t\t{:?}\n\texcess2:\t{:?}\n\tSignature1:\t{:?}\n\n\tsum(k2):\t{:?}",
+                 new_msg,
+                 new_excess,
+                 new_sig,
+                 secp.blind_sum(vec![k2, new_k2], vec![]).unwrap(),
+        );
+
+        // now let's check this "aggregated transaction":
+
+        if true==secp.verify_commit_sum(
+            vec![output1, output2, new_output1, new_output2],
+            vec![input, new_input, excess, new_excess,
+                 commit(0, secp.blind_sum(vec![k2, new_k2], vec![]).unwrap())],
+        ){
+            println!("\ntotal sum balance verify OK:\toutput1 + output2 + output3 + output4 = input1 + input2 + excess1 + excess2 + sum(k2)*G");
+        }else{
+            println!("\ntotal sum balance verify NOK:\toutput1 + output2 + output3 + output4 = input1 + input2 + excess1 + excess2 + sum(k2)*G");
+        }
+    }
 }
