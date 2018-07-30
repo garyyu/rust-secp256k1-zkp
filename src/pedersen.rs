517eb0b05a00abef88420e62d5e3773d4efbe864
Gary Yu <gairy.yu@gmail.com>
2018-07-30 17:45:57 +0800

    fix: ffi secp256k1_rangeproof_info() only support 7 parameters instead of 10

--- src/pedersen.rs
+++ src/pedersen.rs
@@ -621,8 +621,6 @@ impl Secp256k1 {
 		let mut min: u64 = 0;
 		let mut max: u64 = 0;
 
-		let extra_commit = [0u8; 33];
-
 		let success = unsafe {
 			ffi::secp256k1_rangeproof_info(
 				self.ctx,
@@ -632,9 +630,6 @@ impl Secp256k1 {
 				&mut max,
 				proof.proof.as_ptr(),
 				proof.plen as size_t,
-				extra_commit.as_ptr(),
-				0 as size_t,
-				constants::GENERATOR_H.as_ptr(),
 			) == 1
 		};
 		ProofInfo {
